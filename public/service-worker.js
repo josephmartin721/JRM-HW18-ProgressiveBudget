6269ac512dda78c3876c2231154ea05bc02955d8
josephmartin721 <josephmartin721@yahoo.com>
2021-06-13 12:22:40 -0500

    heroku challenges

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/db.js",
     "/styles.css",
+    "/service-worker.js",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
