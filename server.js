6095954bdced94c1abebb05547b895323d00e8c4
josephmartin721 <josephmartin721@yahoo.com>
2021-06-13 13:16:42 -0500

    changed budget_db back to budget

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget_db", {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
